fb1496ee2b93800e704eb27f169b5b63f5545d47
mattrachele <mattrachele@live.com>
2024-12-26 19:58:13 -0600

    Improve pylint score

--- merge_tool.py
+++ merge_tool.py
@@ -27,13 +27,13 @@ import os
 import shutil
 import difflib
 import argparse
-from tqdm import tqdm
-import time
-from colorama import init, Fore, Style
-import re
-import pydoc
 import subprocess
+import pydoc
 import json
+import re
+import time
+from tqdm import tqdm
+from colorama import init, Fore
 
 # Initialize colorama
 init(autoreset=True)
@@ -43,7 +43,11 @@ def version_check(command) -> bool:
     """Check the version of a command."""
     try:
         result = subprocess.run(
-            [command, "--version"], capture_output=True, text=True, shell=True
+            [command, "--version"],
+            capture_output=True,
+            text=True,
+            shell=True,
+            check=True,
         )
         if result.returncode != 0:
             print(f"{command} is not available.")
@@ -72,7 +76,7 @@ def validate_requirements() -> dict:
 
 
 # Validate the requirements
-validated_requirements = validate_requirements()
+valid_requirements = validate_requirements()
 
 
 def strip_whitespace(lines) -> list:
@@ -126,8 +130,8 @@ def view_text_with_less(text) -> None:
             text[i] = Fore.CYAN + line
 
     less_txt = "".join(text)
-    process = subprocess.Popen(["less", "-R"], stdin=subprocess.PIPE)
-    process.communicate(input=less_txt.encode("utf-8"))
+    with subprocess.Popen(["less", "-R"], stdin=subprocess.PIPE) as process:
+        process.communicate(input=less_txt.encode("utf-8"))
 
 
 def open_files_in_vscode_compare(file1, file2) -> None:
@@ -139,7 +143,7 @@ def open_files_in_vscode_compare(file1, file2) -> None:
     abs_file1 = os.path.abspath(file1)
     abs_file2 = os.path.abspath(file2)
 
-    subprocess.run(["code", "--diff", abs_file1, abs_file2], shell=True)
+    subprocess.run(["code", "--diff", abs_file1, abs_file2], shell=True, check=True)
 
 
 def choice_handler(
@@ -291,7 +295,8 @@ def choice_handler(
                     "processed_lines": tmp_merged_mod_lines,
                     "status": "quit-save",
                 }
-            elif user_choice == "10":
+
+            if user_choice == "10":
                 return {"status": "quit"}
 
             if user_choice == "1":
@@ -333,7 +338,7 @@ def choice_handler(
                     "status": "continue",
                 }
             elif user_choice == "7":
-                if not validated_requirements["less"]:
+                if not valid_requirements["less"]:
                     print(
                         "less is not available. Please install it to use this feature. Viewing with pydoc instead."
                     )
@@ -344,7 +349,7 @@ def choice_handler(
                 continue
             elif user_choice == "8":
                 # TODO: Update to allow usage with other editors
-                if not validated_requirements["code"]:
+                if not valid_requirements["code"]:
                     print(
                         "code is not available. Please install it to use this feature."
                     )
@@ -362,15 +367,18 @@ def choice_handler(
                         final_merged_mod_start_line + final_merged_mod_length
                     )
                     break
-                elif confirm == "2":
+
+                if confirm == "2":
                     continue
-                elif confirm == "3":
+
+                if confirm == "3":
                     tmp_merged_mod_lines.extend(new_lines)
                     return {
                         "status": "quit-save",
                         "processed_lines": tmp_merged_mod_lines,
                     }
-                elif confirm == "4":
+
+                if confirm == "4":
                     return {"status": "quit"}
             else:
                 tmp_merged_mod_lines.extend(new_lines)
@@ -390,7 +398,7 @@ def reload_tmp_merged_mod_file(tmp_merged_mod_file) -> int:
     if not os.path.exists(tmp_merged_mod_file):
         return 0
 
-    with open(tmp_merged_mod_file, "r") as tmp_merged_mod:
+    with open(tmp_merged_mod_file, "r", encoding="utf-8") as tmp_merged_mod:
         lines = tmp_merged_mod.readlines()
         last_processed_line = len(lines)
 
@@ -404,7 +412,7 @@ def merge_files(new_mods_file, final_merged_mod_file, confirm_user_choice=False)
     print(f"Mrg: {new_mods_file}\n" f"New: {final_merged_mod_file}")
 
     new_mod_size = os.path.getsize(new_mods_file)
-    final_merged_mod_size = os.path.getsize(final_merged_mod_file)
+    # final_merged_mod_size = os.path.getsize(final_merged_mod_file)
     start_time = time.time()
 
     # Create a temporary file to store the merged contents
@@ -412,9 +420,11 @@ def merge_files(new_mods_file, final_merged_mod_file, confirm_user_choice=False)
     # Check if the temporary file exists and reload the last processed line
     last_processed_line = reload_tmp_merged_mod_file(temp_merged_mod_file)
 
-    with open(new_mods_file, "r") as new_mod, open(
-        final_merged_mod_file, "r"
-    ) as final_merged_mod, open(temp_merged_mod_file, "w") as temp_merged_mod:
+    with open(new_mods_file, "r", encoding="utf-8") as new_mod, open(
+        final_merged_mod_file, "r", encoding="utf-8"
+    ) as final_merged_mod, open(
+        temp_merged_mod_file, "w", encoding="utf-8"
+    ) as temp_merged_mod:
         with tqdm(
             total=new_mod_size, unit="B", unit_scale=True, desc=new_mods_file
         ) as pbar:
@@ -477,7 +487,8 @@ def merge_files(new_mods_file, final_merged_mod_file, confirm_user_choice=False)
                 if choice["status"] == "quit-save":
                     temp_merged_mod.writelines(choice["processed_lines"])
                     return "quit"
-                elif choice["status"] == "quit":
+
+                if choice["status"] == "quit":
                     return "quit"
 
                 # Write the new lines to the temporary file
